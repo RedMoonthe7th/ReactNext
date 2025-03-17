ba12c0e8aa51be38e366d47202c4055f2f827747
Alexander II <alexander.tschanun@icloud.com>
2025-03-17 10:57:54 +0100

    FancyMenu

---
+++ app/cars/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Menu from '../components/ui/menu'
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <div>
+      Cars Page
+    </div>
+  )
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function NotFound({}: Props) {
+  return (
+    <div>Biggo Problemo</div>
+  )
+}
